a29d2bb6a5637c2d18ebf3a36b30508e53320f68
Christophe Rime <christophe.rime@eca-vaud.ch>
2022-12-20 14:00:31 +0100

    Changed port to 9097

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # alertmanager-logger
+
--- main.go
+++ main.go
@@ -22,12 +22,12 @@ func init() {
 
 }
 
-// func writeAlertLog(alertManagerMessage AlertManagerNotificationObject) {
-// 	log.Println("Alert received")
-// 	for _, alert := range alertManagerMessage.Alerts {
-// 		log.Println(alert.Labels.Alertname, alert.Labels.Instance, alert.Status)
-// 	}
-// }
+func writeAlertLog(alertManagerMessage AlertManagerNotificationObject) {
+	log.Println("Alert received")
+	for _, alert := range alertManagerMessage.Alerts {
+		log.Println(alert.Labels.Alertname, alert.Labels.Instance, alert.Status)
+	}
+}
 
 func main() {
 
@@ -45,11 +45,11 @@ func main() {
 
 	app.Post("/logger", func(c *fiber.Ctx) error {
 
-		// var alertManagerMessage AlertManagerNotificationObject
+		var alertManagerMessage AlertManagerNotificationObject
 
-		// c.BodyParser(&alertManagerMessage)
-		// log.Printf("Alert value: %v", alertManagerMessage)
-		// writeAlertLog(alertManagerMessage)
+		c.BodyParser(&alertManagerMessage)
+		log.Printf("Alert value: %v", alertManagerMessage)
+		writeAlertLog(alertManagerMessage)
 
 		log.Println("Alert received")
 		log.Println(string(c.Body()))
@@ -61,7 +61,7 @@ func main() {
 		return c.Status(fiber.StatusForbidden).SendString("Forbidden")
 	})
 
-	err = app.Listen(":9095")
+	err = app.Listen(":9097")
 	if err != nil {
 		panic(err)
 	}
